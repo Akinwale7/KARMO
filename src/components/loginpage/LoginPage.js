6069c2008a620c42f0484677c87ecb211d5c1784
akinwale7 <akinwale.adegboyega@gmail.com>
2019-11-18 19:35:14 -0400

    modified webpack conferguration to split different css file

--- src/components/loginpage/LoginPage.js
+++ src/components/loginpage/LoginPage.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./loginpage.less";
 class LoginPage extends React.Component {
   render() {
-    return <div></div>;
+    return <div className="login_page"></div>;
   }
 }
 export default LoginPage;
